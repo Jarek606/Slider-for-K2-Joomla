2f2d0062aada5ed59bb408cabb798a8a13ec88bc
Jarek606 <jarekdawidowicz+github@gmail.com>
2013-04-08 15:09:26 +0200

    add user settings

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,9 +6,8 @@ $document->addStyleSheet(JURI::base() . 'modules/mod_sliderjd/themes/default/def
 $document->addStyleSheet(JURI::base() . 'modules/mod_sliderjd/themes/light/light.css');
 $document->addStyleSheet(JURI::base() . 'modules/mod_sliderjd/themes/dark/dark.css');
 $document->addStyleSheet(JURI::base() . 'modules/mod_sliderjd/css/nivo-slider.css');
-
 ?>
-	<div class="slider-wrapper theme-default">
+	<div class="slider-wrapper theme-<? echo $params->get('theme'); ?>" style="width:<? echo $params->get('width'); ?>;">
 		<div id="slider" class="nivoSlider">
 			<?php foreach ($images as $img){ ?>
 				<img src="<? echo JURI::base().'media/k2/items/src/'.md5('Image'.$img->id).'.jpg';?>" data-thumb="<? echo JURI::base().'media/k2/items/src/'.md5('Image'.$img->id).'.jpg';?>" alt="" title="<? echo $img->introtext; ?>" />
@@ -20,7 +19,7 @@ $document->addScript(JURI::base() . 'modules/mod_sliderjd/js/jquery-1.9.0.min.js
 $document->addScript(JURI::base() . 'modules/mod_sliderjd/js/jquery.nivo.slider.js');
 ?>
 <script type="text/javascript">
-	j$132(window).load(function() {
+	$(window).load(function() {
 		$('#slider').nivoSlider();
 	});
 </script>
